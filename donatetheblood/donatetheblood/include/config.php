f6f05fa0b05523d60fcaaae2be9658d9c36de848
Radhika Udaykumar Nair <109176048+Radhika-unair@users.noreply.github.com>
2024-03-30 13:41:23 +0530

    edited

--- donatetheblood/donatetheblood/include/config.php
+++ donatetheblood/donatetheblood/include/config.php
@@ -1,5 +1,5 @@
 <?php
 
-$connection = mysqli_connect("localhost","root","","doantetheblood");
+$connection = mysqli_connect("localhost","root","","doantetheblood") or die("Database is not connected.".mysqli_connect_error());
 
 ?>
