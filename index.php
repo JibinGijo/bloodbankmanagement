e0c50514966b9aed3a2fd648117b09a490a18b3f
Radhika Udaykumar Nair <109176048+Radhika-unair@users.noreply.github.com>
2024-04-18 22:44:07 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <title>Admin Login</title>
+</head>
+<body>
+<div id="full">
+    <div id="inner-full">
+        <div id="header">Header</div> 
+        <div id="header">Body</div>    
+        <div id="header">Footer</div> 
+</div>
+</div>         
+</body>
+</html>
