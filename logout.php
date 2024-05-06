5fd653696e13cd6f5b6c13989d81135bf0db2364
Jibin Gijo <138276454+JibinGijo@users.noreply.github.com>
2024-05-06 15:28:11 +0000

    logout_update

--- logout.php
+++ logout.php
@@ -1,6 +1,7 @@
- <?php
- session_start();
- $un=$_SESSION['un'];
+<?php
+session_start();
+$un = $_SESSION['un'];
 session_destroy();
- header('Location:index.php');
- ?>
+echo '<script>alert("You have been logged out.");</script>';
+header('Location:index.php');
+?>
